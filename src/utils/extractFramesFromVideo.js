54cf2f54fdefc5f0d714f9ce4f7fe185084d42db
Umar Hansa <umar.hansa@gmail.com>
2023-02-02 14:15:14 +0000

    fix

--- src/utils/extractFramesFromVideo.js
+++ src/utils/extractFramesFromVideo.js
@@ -1,10 +1,20 @@
 import ffmpeg from 'fluent-ffmpeg';
+import { dirname } from 'path';
+import { existsSync, mkdirSync } from 'fs';
 
-export async function extractFramesFromVideo(inputFilepath, outputFilepath, frameRate) {
-
+export async function extractFramesFromVideo(
+  inputFilepath,
+  outputFilepath,
+  frameRate
+) {
   await new Promise((resolve, reject) => {
+    // Ensure output path exists
+    const filePath = dirname(outputFilepath);
+    if (!existsSync(filePath)) {
+      console.log('Path does not exist');
+      mkdirSync(filePath);
+    }
     ffmpeg()
-
       // Specify the filepath to the video
       .input(inputFilepath)
 
